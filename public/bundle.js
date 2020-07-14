49a9731dbbca2519722d95dc7c6ffcad27687e55
shulajz <shulajz@gmail.com>
2020-07-14 20:35:43 +0300

    'change'

--- public/bundle.js
+++ public/bundle.js
@@ -38127,9 +38127,18 @@
 
 	var Examples = function Examples(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'Examples Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples Component'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to Examples page'
+	    )
 	  );
 	};
 
